307c9e125e2790768d40a5b82e82e0293b831851
Muhammad Ashir Khan <113202924+muhammadAsh1r@users.noreply.github.com>
2025-09-15 11:03:19 +0500

    Update authSlice.js

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -2,7 +2,7 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 // Base API URL
-const API_URL = "http://127.0.0.1:8000/api/";
+const API_URL = "https://students-expense.onrender.com/api/";
 
 // --------------------
 // Async Thunks
