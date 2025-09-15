eb526e5c4ea3be4871e2791c4323f684df9b195e
Muhammad Ashir Khan <113202924+muhammadAsh1r@users.noreply.github.com>
2025-09-15 11:04:32 +0500

    Update friendsSlice.js

--- frontend/src/features/friends/friendsSlice.js
+++ frontend/src/features/friends/friendsSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000/api"; // backend URL
+const API_URL = "https://students-expense.onrender.com/api/"; // backend URL
 
 // --------------------------
 // Async Thunks
